22f6835a0d1795bf2922b6759e60aff4270f5c59
WWK Build <wwk.build.2026@gmail.com>
2026-04-07 19:04:32 +0000

    BCC hello@womenwhoknow.ca on every delivered report

--- api/send-report.js
+++ api/send-report.js
@@ -51,6 +51,7 @@ export default async function handler(req, res) {
     const { error: sendError } = await resend.emails.send({
       from: "Anitta Hamming <hello@womenwhoknow.ca>",
       to: email,
+      bcc: "hello@womenwhoknow.ca",
       subject: "Your WWK Founder Benchmark Assessment Report",
       html: buildReportEmail(name, report, printUrl),
     });
